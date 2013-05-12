3c9686fb2283aca439fffe6df27886035f99ee8b
Philip Luppens <philip.luppens@gmail.com>
2013-05-12 15:28:15 +0200

    Added versioning support.

--- src/main/java/org/nsesa/server/repository/AmendmentContainerRepository.java
+++ src/main/java/org/nsesa/server/repository/AmendmentContainerRepository.java
@@ -3,6 +3,7 @@ package org.nsesa.server.repository;
 import org.nsesa.server.domain.AmendmentContainer;
 import org.nsesa.server.domain.Document;
 import org.nsesa.server.domain.Person;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,7 +20,13 @@ public interface AmendmentContainerRepository extends PagingAndSortingRepository
 
     List<AmendmentContainer> findByDocumentAndPerson(Document document, Person person);
 
+    List<AmendmentContainer> findByAmendmentContainerID(String amendmentContainerID);
+
     List<AmendmentContainer> findByDocument(Document document);
 
-    AmendmentContainer findByAmendmentContainerID(String amendmentContainerID);
+    List<AmendmentContainer> findLatestByDocument(Document document);
+
+    AmendmentContainer findByRevisionID(String revisionID);
+
+    List<AmendmentContainer> findByAmendmentContainerIDOrderByCreationDateDesc(String amendmentContainerID, Pageable pageable);
 }
